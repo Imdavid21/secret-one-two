550595d0d59335d990abab926fde6274e7025edd
Imdavid21 <84083101+Imdavid21@users.noreply.github.com>
2024-08-27 19:05:39 +0530

    Update Hume.tsx

--- components/logos/Hume.tsx
+++ components/logos/Hume.tsx
@@ -5,8 +5,8 @@ export type HumeLogoProps = ImgHTMLAttributes<HTMLImageElement>;
 export default function HumeLogo(props: HumeLogoProps) {
   return (
     <img
-      src="https://as2.ftcdn.net/v2/jpg/04/85/76/47/1000_F_485764777_m7Mz1HJDEFVHfIwIZ4dBtNzbOc2Kuewq.jpg"
-      alt="Hume Logo"
+      src="https://freeimage.host/i/dhQqfRe"
+      alt="Logo"
       width="106"
       height="25"
       {...props}
